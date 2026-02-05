ca532ac6f91be042782cb69ccbffe576c2bc6cfc
Saktheeswaran7103 <saktheeswaran7103@gmail.com>
2026-02-05 16:53:41 +0530

    views changed

--- blog/views.py
+++ blog/views.py
@@ -1,89 +1,131 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render, redirect
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect
+
 from .models import Post, Category
 from .forms import PostForm, CommentForm
 
 
-
-
-class PostListView(ListView):
+# =========================
+# POST LIST (ONLY USER POSTS)
+# =========================
+class PostListView(LoginRequiredMixin, ListView):
     model = Post
-    template_name = 'blog/post_list.html'
-    context_object_name = 'posts'
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
     paginate_by = 9
+
     def get_queryset(self):
-        return Post.objects.filter(published=True)
+        # Superuser can see all posts
+        if self.request.user.is_superuser:
+            return Post.objects.filter(published=True)
+
+        # Normal user sees ONLY their posts
+        return Post.objects.filter(
+            author=self.request.user,
+            published=True
+        )
 
 
+# =========================
+# POSTS BY CATEGORY
+# =========================
 class PostListByCategory(PostListView):
     def get_queryset(self):
-        cat_name = self.kwargs.get('name')
-        return Post.objects.filter(category__name__iexact=cat_name, published=True)
+        category_name = self.kwargs.get("name")
+
+        if self.request.user.is_superuser:
+            return Post.objects.filter(
+                category__name__iexact=category_name,
+                published=True
+            )
+
+        return Post.objects.filter(
+            author=self.request.user,
+            category__name__iexact=category_name,
+            published=True
+        )
 
 
-class PostDetailView(DetailView):
+# =========================
+# POST DETAIL
+# =========================
+class PostDetailView(LoginRequiredMixin, DetailView):
     model = Post
-    template_name = 'blog/post_detail.html'
-    context_object_name = 'post'
+    template_name = "blog/post_detail.html"
+    context_object_name = "post"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        if self.request.user.is_superuser:
+            return queryset
+
+        # Prevent viewing other users' posts
+        return queryset.filter(author=self.request.user)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['comment_form'] = CommentForm()
+        context["comment_form"] = CommentForm()
         return context
+
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
-        if not request.user.is_authenticated:
-            return redirect('blog:login')
         form = CommentForm(request.POST)
+
         if form.is_valid():
             comment = form.save(commit=False)
             comment.user = request.user
             comment.post = self.object
             comment.save()
             return HttpResponseRedirect(self.object.get_absolute_url())
-        context = self.get_context_data(object=self.object, comment_form=form)
-        return self.render_to_response(context)
+
+        return self.render_to_response(
+            self.get_context_data(comment_form=form)
+        )
 
 
+# =========================
+# CREATE POST
+# =========================
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     form_class = PostForm
-    template_name = 'blog/post_form.html'
+    template_name = "blog/post_form.html"
+
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
 
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Post
-    form_class = PostForm
-    template_name = 'blog/post_form.html'
-    def test_func(self):
-        return self.get_object().author == self.request.user
-
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Post
-    success_url = reverse_lazy('blog:post_list')
-    def test_func(self):
-        return self.get_object().author == self.request.user
 
 
+
+# =========================
+# SIGNUP VIEW
+# =========================
 def signup_view(request):
-    if request.method=='POST':
-        form=UserCreationForm(request.POST)
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
         if form.is_valid():
-            user=form.save()
-            login(request,user)
-            return redirect('blog:login')
-        else:
-            print(form.errors)
+            user = form.save()
+            login(request, user)
+            return redirect("blog:post_list")
     else:
-        form=UserCreationForm()
-    return render(request,'blog/registration/signup.html',{'form':form})
+        form = UserCreationForm()
 
-# Create your views here.
+    return render(
+        request,
+        "blog/registration/signup.html",
+        {"form": form},
+    )
